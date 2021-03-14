df6c941dcdd66dadda43759e54ab571dc2e80e08
jeanpaulHT <jean.huby@utec.edu.pe>
2021-03-13 22:03:43 -0500

    xd

--- include/parser.hpp
+++ include/parser.hpp
@@ -5,4 +5,25 @@
 #ifndef ASDFASDF_PARSER_HPP
 #define ASDFASDF_PARSER_HPP
 
+#include <vector>
+
+using Rules = std::vector<int>;
+using Rules = std::vector<int>;
+
+
+
+
+class SLRparser{
+private:
+
+
+public:
+
+    explicit SLRparser (const Rules& rules, ID start);
+
+    bool
+    parse (std::string_view s);
+};
+
+
 #endif //ASDFASDF_PARSER_HPP
