c83d05e7db23374cec05865d70086a6a829a8636
GyLord_69 <esteban.villacorta@utec.edu.pe>
2021-03-22 03:09:48 -0500

    Scanner now delegates error handling to parser

--- include/scanner.hpp
+++ include/scanner.hpp
@@ -14,10 +14,14 @@ namespace Grammar
 {
     using namespace std::string_view_literals;
 
+    using View = std::string_view;
+    using Type = unsigned;
+
     constexpr static unsigned NUM_TOKENS = 20;
-    enum Token : unsigned
+
+    enum Token : Type
     {
-        ACHT = 1,
+        ACHT = 0,
         DREI,
         EIN,
         ELF,
@@ -37,36 +41,70 @@ namespace Grammar
         ZWAN,
         ZWEI,
         ZWOELF,
-        ERR_BIT = (1u << 31u),
-        NONE = 0u,
+        $end = ~((Type) 0) - 1,
+        $error
+    };
+
+
+    constexpr static unsigned NUM_NON_TERM = 19;
+    constexpr static Type PROD_MASK = 1 << (sizeof(Type) - 1);
+
+    enum Production : Type
+    {
+        START = PROD_MASK,
+        S,
+        Z1,
+        Z2,
+        Z3,
+        Z4,
+        Z5,
+        Z6,
+        Z7,
+        Z8,
+        Z9,
+        Z10,
+        Z11,
+        Z12,
+        Z13,
+        Z14,
+        Z613,
+        Z1278911,
+        U,
     };
 
     constexpr static std::array TOKEN_VAL = {
-        0xfffffff, 8, 3, 1, 11, 5, 100, 9, 6, 6, 7, 7,
+        8, 3, 1, 11, 5, 100, 9, 6, 6, 7, 7,
         10, 1000, 0, 4, 10, 10, 2, 2, 12
     };
 
+    using Value = decltype(TOKEN_VAL)::value_type;
+
     constexpr static std::array TOKEN_STR = {
-        ""sv, "acht"sv, "drei"sv, "ein"sv, "elf"sv,
+        "acht"sv, "drei"sv, "ein"sv, "elf"sv,
         "fuenf"sv, "hundert"sv, "neun"sv, "sech"sv,
         "sechs"sv, "sieb"sv, "sieben"sv, "ssig"sv,
         "tausend"sv, "und"sv, "vier"sv, "zehn"sv,
         "zig"sv, "zwan"sv, "zwei"sv, "zwoelf"sv,
     };
 
-    constexpr static auto strOf (unsigned token) noexcept
+
+    constexpr static View strOf (Type token) noexcept
     {
+        if (token == $end)
+            return "END OF FILE"sv;
         return TOKEN_STR[token];
     }
 
-    constexpr static auto valueOf (unsigned token) noexcept
+    constexpr static Value valueOf (Type token) noexcept
     {
         return TOKEN_VAL[token];
     }
 
-    using Type = std::underlying_type_t<Token>;
-    using Value = decltype(TOKEN_VAL)::value_type;
-    using View = std::string_view;
+    constexpr static Production indexOfProduction (unsigned index)
+    {
+        return (Production)(index | PROD_MASK);
+    }
+
 }
 
 constexpr auto beginsWith (Grammar::View s, Grammar::View e)
@@ -74,77 +112,32 @@ constexpr auto beginsWith (Grammar::View s, Grammar::View e)
     return s.substr(0, e.size()) == e;
 };
 
-struct Lexer
+namespace Lexer
 {
-private:
-    template<class Compare>
-    constexpr auto matcher (std::string_view s, Compare func)
+    using namespace Grammar;
+    using namespace std;
+
+    struct Item
     {
-        using namespace Grammar;
-        return [s = View(s), f = std::move(func)] (View a, View b)
+        Token token;
+        View strVal;
+        Value value;
+    };
+
+
+    constexpr static auto matcher (View s)
+    {
+        return [s = View(s)] (View a, View b)
         {
-            auto diff_pos = std::mismatch(begin(s), end(s), begin(b)).first;
-            auto new_size = (std::size_t) std::distance(begin(s), diff_pos);
-            return s.substr(0, f(new_size, a.size()));
+            auto diff_pos = mismatch(begin(s), end(s), begin(b)).first;
+            auto new_size = (size_t) distance(begin(s), diff_pos);
+            return s.substr(0, max(new_size, a.size()));
         };
     }
 
-public:
-    struct Item
+    constexpr static auto lexToken (View s)
     {
-        Grammar::Type error = 0;
-        Grammar::Token token = Grammar::NONE;
-
-        constexpr bool hasError ()
-        { return error & (unsigned) Grammar::ERR_BIT; }
-
-        constexpr Grammar::Value value () const
-        {
-            return Grammar::valueOf(token);
-        }
-
-        constexpr Grammar::View str () const
-        {
-            return Grammar::strOf(token);
-        }
-
-        template<class ... Args>
-        constexpr static auto Error (Args ... args)
-        {
-            using namespace Grammar;
-            Type err = (ERR_BIT | ... | (1u << ((Type) args - 1)));
-
-            return Item{
-                .error = err,
-                .token = NONE
-            };
-        }
-
-        constexpr static auto Valid (Grammar::Token token)
-        {
-            using namespace Grammar;
-            return Item{
-                .error = (Type) NONE,
-                .token = token
-            };
-        }
-    };
-
-    Grammar::View stream;
-    int position = 0;
-
-public:
-    explicit Lexer (std::string_view s)
-        : stream(s), position(0)
-    {}
-
-    /*constexpr*/
-    auto lexToken (std::string_view s)
-    {
-        using namespace Grammar;
-        using namespace std;
-
-        if (s.empty()) return Item::Valid(NONE);
+        if (s.empty()) return Item{ $end, strOf($end), 0 };
 
         auto greater_than_s = [s = View(s)] (const auto& a) { return a > s; };
         auto gt_eq = greater_equal<>();
@@ -161,95 +154,50 @@ public:
         if (rev == rtEnd || (*rev)[0] != s[0])
             matchFst++, matchLst++;
 
-        auto bestFunc = matcher(s, [] (auto a, auto b) { return max(a, b); });
-        auto match = accumulate(matchFst, matchLst, noMatch, bestFunc);
+        auto matchFunc = matcher(s);
+        auto match = accumulate(matchFst, matchLst, noMatch, matchFunc);
 
         auto fs = lower_bound(tBegin, tEnd, match);
         if (fs != tEnd && *fs == match)
         {
             auto token = static_cast<Token>(distance(begin(TOKEN_STR), fs));
-            auto item = Item::Valid(static_cast<Token>(token));
-            position += item.str().size();
+            auto strVal = s.substr(0, match.size());
+            auto item = Item{ token, strVal, valueOf(token) };
             return item;
         }
-
-        auto commonFunc = matcher(s, [] (auto a, auto b) { return min(a, b); });
-        auto common = accumulate(matchFst, matchLst, s, commonFunc);
-
-        auto item = Item::Error(NONE);
-        if (common.empty()) return item;
-
-        auto fix = lower_bound(tBegin, tEnd, common);
-        for (; fix != end(TOKEN_STR) && beginsWith(*fix, common); ++fix)
-        {
-            auto bitToSet = (Type) distance(tBegin, fix);
-            item.error |= ((Type) 1u) << bitToSet;
-        }
-        position += common.size();
-        return item;
+        return Item{ $error, s.substr(0, 1) };
     }
 
-    struct LexerError : std::exception
+    static std::vector<Item> lex (View stream)
     {
-        std::string msg;
-        LexerError (std::string_view s) : msg(s) {}
-        const char* what () const throw()
-        {
-            return msg.c_str();
-        }
-    };
-
-    [[noreturn]]
-    void exitWithError (Item fault, Grammar::View str)
-    {
-        constexpr int prefixSize = 10, trailSize = 12;
-        std::stringstream errStream;
-
-        errStream << "On position " << position << " of input: \n";
-        errStream << "Unexpected character '" << stream[position]
-                  << "' during lex attempt of: " << str << "\n\n";
-
-        auto hasThreeDots = position >= prefixSize;
-        auto prefixBegin = std::max(0, position - prefixSize);
-        auto prefix = stream.substr(prefixBegin, position - prefixBegin);
-        auto trail = stream.substr(position, trailSize);
-        errStream << (position >= prefixSize ? "..." : "")
-                  << prefix << trail
-                  << (trail.size() < trailSize ? "" : "...")
-                  << "\n";
-
-        for (int i = 0; i < prefix.size() + 3 * hasThreeDots; ++i)
-            errStream << '-';
-        errStream << "^\n\n";
-
-        bool found = false;
-        for (unsigned i = 0; i < Grammar::NUM_TOKENS; ++i)
-        {
-            if (fault.error & (1u << i))
-            {
-                found = true;
-                errStream << "Possible fix: " << Grammar::strOf(i + 1) << "\n";
-            }
-        }
-        if (!found) errStream << "No obvious fixes available\n";
-        throw LexerError(errStream.str());
-    }
-
-    std::vector<Item> lex ()
-    {
-        Item item;
         std::vector<Item> lexemes;
+        unsigned position = 0;
         while (true)
         {
-            auto pPos = position;
-            item = lexToken(stream.substr(position));
-            auto diff = position - pPos;
-            if (!item.token)
+            auto item = lexToken(stream.substr(position));
+            if (item.token == Grammar::$end)
             {
-                if (!item.hasError()) return lexemes;
-                exitWithError(item, stream.substr(pPos, diff));
+                lexemes.push_back(item);
+                return lexemes;
             }
-            lexemes.emplace_back(item);
+            if (item.token == Grammar::$error)
+            {
+                position++;
+                if (lexemes.empty() || lexemes.back().token != Grammar::$error)
+                {
+                    lexemes.push_back(item);
+                }
+                else
+                {
+                    auto& back = lexemes.back().strVal;
+                    back = Grammar::View{ back.data(), back.size() + 1 };
+                }
+            }
+            else
+            {
+                lexemes.push_back(item);
+                position += item.strVal.size();
+            }
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include "include/parser.hpp"
+#include "include/scanner.hpp"
 
 int main ()
 {
     using namespace Grammar;
-//    int a[5] = {1, 2, 3, 4, 5};
-//    int b[5] = {1, 1, 1, 1, 1};
-//    int i;
-//    a[a[i]] = b[i=2];
-//    for (auto ii : a)
-//        std::cout << ii << ' ';
+    auto i = Lexer::lex("zwoelfasdfzehnokokzehnknvdnvdkn");
+    return 0;
 }
